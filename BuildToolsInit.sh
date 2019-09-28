6da9684698e57bac2709bcf4d2becc66bb4d24d5
xubenji <459547070@qq.com>
2019-09-28 17:38:39 +0800

    init the build tool

--- BuildToolsInit.sh
+++ BuildToolsInit.sh
@@ -3,25 +3,53 @@
 #2019/09/20
 #You can use this script to init the basic build tools
 #I recommd you use a clean Ubuntu 18.
-#This script build OVMF.fd by default.
-#If you don't want to build the OVMF.fd module, you have the OVMF.fd and you know how to run qemu with OVFM.fd.
-#You can write NOOVMF=1
-#if you want to build all. You can run script like this: bash BuildToolsInit.sh
-#if you want to complie the edk2 basetools using your physical machine. you can run this script like this 
+#This script only build bochs by default.
+#You can run this script like this:
+#>:bash BuildToolsInit.sh
+#If you want to build all of they. you can change the parameter like this:
+#	BOCHS=1
+#	BOCHSDEBUGGER=0
+#	BOCHSGDB=0
+#	EDK=1
+#	OVMF=1
+#	DOCKER=1
+#If you want to build edk2 and don't want to build the bochs.
+#You can change parameter like this:
+#	BOCHS=0
+#	BOCHSDEBUGGER=0
+#	BOCHSGDB=0
+#	EDK=1
+#	OVMF=1
+#	DOCKER=0
+#If you want to only build bochs with gdb debugger. 
+#You can change parameter like this:
+#	BOCHS=1
+#	BOCHSDEBUGGER=0
+#	BOCHSGDB=1
+#	EDK=0
+#	OVMF=0
+#	DOCKER=0
+#The BOCHSDEBUGGER and BOCHSGDB depends on BOCHS. if BOCHS=0, they are meaningless.
+#In the similar way, The OVMF depends on EDK. Because OVMF.fd is a firmware of UEFI. 
+#If EDK=0, the OVMF is meaningless.
+#If you don't know the docker. you don't need to care about the parameter DOCKER.
+#If you want to complie the edk2 basetools using your physical machine. you can run this script like this 
 #>:bash BuildToolsInit.sh -nodocker
+#Good lock!
 
-NOOVMF=0
-NOBOCHS=0
+BOCHS=0
 BOCHSDEBUGGER=0
 BOCHSGDB=0
-NOEDK=1
-NODOCKER=0
 
-echo
+EDK=0
+OVMF=1
+
+DOCKER=1
+
 while [ -n "$1" ]
 do	
     case "$1" in
-	-nodocker) NODOCKER=1 ;;
+	-nodocker) DOCKER=0 ;;
         -repair) rm -r -f edk2 
 		 mv /etc/apt/sources.list /etc/apt/sources.bak
 		 cp ./ToolSource/sources.list /etc/apt/sources.list
@@ -39,7 +67,7 @@ do
 done
 
 #check if the program in docker
-if [ $NODOCKER -eq 0 ];then
+if [ $DOCKER -eq 1 ];then
 	sudo apt-get install docker.io -y
 
 	#install application for CentOS-like system
@@ -63,17 +91,17 @@ apt-get update
 
 
 #install bochs with debugger
-if [ $NOBOCHS -eq 0 ];then
+if [ $BOCHS -eq 1 ];then
 apt-get install sudo -y
 sudo apt-get install dpkg-dev -y
 sudo apt-get -y install gcc
 #sudo apt-get -y install g++
 sudo apt-get install xorg-dev -y
 sudo apt-get install libgtk2.0-dev -y
-rm boch*
-rm boch* -r
+rm boch* -f
+rm boch* -r -f 
 apt-get source bochs -y
-rm bochs-2.* -r
+rm bochs-2.* -r -f
 tar -vxf bochs_2.*.or*
 cd bochs-2.*
 ./configure --enable-debugger --enable-disasm
@@ -84,12 +112,11 @@ cp ../ToolSource/Makefile.bochsdebugger ./Makefile
 script -a "../output.bochs" -c "make" 
 cp ../ToolSource/BIOS-* ./bios/
 cp ../ToolSource/VGABIOS-* ./bios/
-rm bochs_*
 fi
 
 pwd
 
-if [ $NOEDK -eq 0 ];then
+if [ $EDK -eq 1 ];then
 cd /dolphin
 #install sudo 
 apt-get install sudo -y
@@ -132,7 +159,7 @@ sudo update-alternatives --config gcc
 gcc -v
 
 #compile the OVMF.fd
-if [ $NOOVMF -eq 0 ];then
+if [ $OVMF -eq 1 ];then
 rm Conf/target.txt
 cp ../ToolSource/target.txt Conf/target.txt
 BaseTools/BuildEnv 
@@ -146,7 +173,7 @@ fi
 fi
 
 #Check if the compilation is successful
-if [ $NOEDK -eq 0 ];then
+if [ $EDK -eq 1 ];then
 grep "tests in" ../output.tools >/dev/null
 if [ $? -eq 0 ]; then
     echo -e "[\033[32m Build base tools success \033[0m]"
@@ -164,7 +191,7 @@ grep "\- Done \-" ../output.ovmf >/dev/null
 	fi
 fi
 fi
-if [ $NOBOCHS -eq 0 ]; then
+if [ $BOCHS -eq 1 ]; then
 pwd
 grep "g++ \-o bxcommit \-g \-O2 \-D_FILE_OFFSET_BITS=64 \-D_LARGE_FILES -pthread \-fPIC misc/bxcommit.o" ../output.bochs >/dev/null
 	if [ $? -eq 0 ]; then
@@ -189,5 +216,6 @@ rm -f ../failed.txt
 rm -f ../output.tools
 rm -f ../output.ovmf
 rm -f ../output.bochs
+rm bochs_2* -f
 
 exit
